ebdbc37da22829443a2b676b28dcd4312f3ddb3e
oluoko <brianotieno586@gmail.com>
2024-09-03 14:31:53 +0300

    Geno Safaris Website

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dontenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+dontenv.config();
+const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoutes.js");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
