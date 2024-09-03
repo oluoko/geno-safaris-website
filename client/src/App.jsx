ebfec7f3c99cea5109b3d65862349b0bf022e33b
oluoko <brianotieno586@gmail.com>
2024-09-03 15:03:48 +0300

    Geno Safaris Website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen grid md:flex grid-col justify-center md:justify-around items-center shadow-lg border-2 rounded-xl bg-gray-500">
+      App
+    </div>
+  );
 };
 
 export default App;
