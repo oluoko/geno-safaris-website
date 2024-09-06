129b05a246e95569d674c3b589cab7aefe101427
oluoko <brianotieno586@gmail.com>
2024-09-06 05:55:40 +0300

    Geno Safaris Website: Updated App.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,23 @@ import Checkout from "./Screens/Checkout";
 import LoginScreen from "./Screens/LoginScreen";
 import RegisterScreen from "./Screens/RegisterScreen";
 import ProfileScreen from "./Screens/ProfileScreen";
+import { ScreenSizeProvider } from "./Components/ScreenSizeContext";
 
 const App = () => {
   return (
-    <Router>
-      <div className="w-full h-screen grid md:flex grid-col justify-center md:justify-around items-center">
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/checkout/:id" element={<Checkout />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/signup" element={<RegisterScreen />} />
-          <Route path="/profile" element={<ProfileScreen />} />
-        </Routes>
-      </div>
-    </Router>
+    <ScreenSizeProvider>
+      <Router>
+        <div className="w-full h-screen grid md:flex grid-col justify-center md:justify-around items-center">
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/checkout/:id" element={<Checkout />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/signup" element={<RegisterScreen />} />
+            <Route path="/profile" element={<ProfileScreen />} />
+          </Routes>
+        </div>
+      </Router>
+    </ScreenSizeProvider>
   );
 };
 
