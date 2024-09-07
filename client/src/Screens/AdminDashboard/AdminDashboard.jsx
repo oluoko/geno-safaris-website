95022e83bc16e8d6c46b41d1704d9ac3ee35bc43
oluoko <brianotieno586@gmail.com>
2024-09-07 16:03:12 +0300

    Geno Safaris Website: Updated AdminDashboard.jsx file

--- client/src/Screens/AdminDashboard/AdminDashboard.jsx
+++ client/src/Screens/AdminDashboard/AdminDashboard.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import './css/style.css';
+import "./css/style.css";
 
-import './charts/ChartjsConfig';
+import "./charts/ChartjsConfig";
 
 // Import pages
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 
 const AdminDashboard = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
+  }, [location.pathname]); // triggered on route change
   return <div>AdminDashboard</div>;
 };
 
