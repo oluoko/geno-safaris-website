958f433ddb85859ae6259f99056506147eaadeac
oluoko <brianotieno586@gmail.com>
2024-09-07 16:02:32 +0300

    Geno Safaris Website: Updated AdminDashboard.jsx file

--- client/src/Screens/AdminDashboard/AdminDashboard.jsx
+++ client/src/Screens/AdminDashboard/AdminDashboard.jsx
@@ -1,4 +1,16 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+
+import './charts/ChartjsConfig';
+
+// Import pages
+import Dashboard from './pages/Dashboard';
 
 const AdminDashboard = () => {
   return <div>AdminDashboard</div>;
