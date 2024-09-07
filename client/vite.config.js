f1bc3bb81e0dc564a9514e31f7ccf381c513e6a5
oluoko <brianotieno586@gmail.com>
2024-09-07 16:33:21 +0300

    Geno Safaris Website

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc"; // Using the SWC version of React plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    "process.env": process.env,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@tailwindConfig": path.resolve(__dirname, "tailwind.config.js"),
+    },
+  },
+  optimizeDeps: {
+    include: ["@tailwindConfig"],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
